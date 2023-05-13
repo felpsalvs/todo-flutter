9f5a7708dea0c41704af631f36ad264187aab04f
Felipe Alves <profelipealves@outlook.com>
2023-05-13 10:34:33 -0300

    ✨ feat: add functions for dates

--- lib/ui/add_task_bar.dart
+++ lib/ui/add_task_bar.dart
@@ -1,25 +1,68 @@
 import 'package:agendamento/ui/theme.dart';
+import 'package:agendamento/ui/widgets/input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
-class AddTaskPage extends StatelessWidget {
+class AddTaskPage extends StatefulWidget {
   const AddTaskPage({Key? key}) : super(key: key);
 
   @override
+  _AddTaskPageState createState() => _AddTaskPageState();
+}
+
+class _AddTaskPageState extends State<AddTaskPage> {
+  DateTime _selectedDate = DateTime.now();
+  String _endTime='9:30 PM';
+  String _startTime = DateFormat('hh:mm a').format(DateTime.now()).toString();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: context.theme.backgroundColor, 
-      appBar: _appBar(context), 
-      body: Container(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          children: [
-            Text(
-              'Adicionar Agendamento',
-              style: headingStyle,
-            )
-          ],)
-      ));
+        backgroundColor: context.theme.backgroundColor,
+        appBar: _appBar(context),
+        body: Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              children: [
+                Text(
+                  'Adicionar Agendamento',
+                  style: headingStyle,
+                ),
+                MyInputField(title: 'Titulo', hint: 'Digite seu titulo'),
+                MyInputField(title: 'Observação', hint: 'Digite sua observação'),
+                MyInputField(
+                  title: 'Data',
+                  hint: DateFormat.yMd().format(_selectedDate),
+                  widget: IconButton(
+                    icon:
+                        Icon(Icons.calendar_today_outlined, color: Colors.grey),
+                    onPressed: () {
+                      print('oi');
+                      _getDateFromUser();
+                    },
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: MyInputField(
+                        title: 'Data de inicio',
+                        hint: _startTime,
+                        widget: IconButton(
+                          onPressed: (){
+
+                          },
+                          icon: Icon(
+                            Icons.access_time_rounded,
+                            color:Colors.grey,
+                          )
+                        )
+                      )
+                    )
+                  ]
+                )
+              ],
+            )));
   }
 
   _appBar(BuildContext context) {
@@ -48,5 +91,22 @@ class AddTaskPage extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  _getDateFromUser() async {
+    DateTime? _pickerDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2121));
+
+    if (_pickerDate != null) {
+      setState(() {
+        _selectedDate = _pickerDate;
+        print(_selectedDate);
+      });
+    } else {
+      print('oi');
+    }
   }
 }
--- lib/ui/widgets/input_field.dart
+++ lib/ui/widgets/input_field.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../theme.dart';
 
 class MyInputField extends StatelessWidget {
   final String title;
@@ -17,11 +20,57 @@ class MyInputField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(top:16),
         child: Column(
-      children: [Text(
-        title,
-        style: titleStyle,
-      )],
-    ));
+          crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: titleStyle,
+        ),
+        Container(
+          height: 52,
+          margin: EdgeInsets.only(top:8.0),
+          padding: EdgeInsets.only(left: 14),
+          decoration: BoxDecoration(
+            border: Border.all(
+          color: Colors.grey,
+          width: 1.0,
+        ),
+        borderRadius: BorderRadius.circular(12)
+          ),
+          child: Row(
+            children:[
+              Expanded(
+                child: TextFormField(
+                  readOnly: widget==null?false:true,
+                  autofocus: false,
+                  cursorColor: Get.isDarkMode?Colors.grey[100]:Colors.grey[700],
+                  controller: controller,
+                  style: subTitleStyle,
+                  decoration: InputDecoration(
+                    hintText: hint,
+                    hintStyle: subTitleStyle,
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color:context.theme.backgroundColor,
+                        width: 0
+                      )
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color:context.theme.backgroundColor,
+                        width: 0
+                    )
+                  ),
+                )
+              )
+            ),
+            widget==null?Container():Container(child:widget)
+          ],
+        ),
+      ),
+    ])
+  );
   }
 }

9649753cb8e47e69cdddfffd4d59b2dd9e7e5a7c
Felipe Alves <profelipealves@outlook.com>
2023-05-15 19:19:59 -0300

    feat: update format code

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,32 @@ import 'package:agendamento/services/theme_services.dart';
 import 'package:agendamento/ui/home_page.dart';
 import 'package:agendamento/ui/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Adicione esta linha:
-  await DBHelper.initDatabase();
-
-  runApp(MyApp());
+WidgetsFlutterBinding.ensureInitialized();
+// Adicione esta linha:
+await DBHelper.initDatabase();
+runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+const MyApp({Key? key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-        title: 'Agendamento',
-        debugShowCheckedModeBanner: false,
-        theme: Themes.light,
-        darkTheme: Themes.dark,
-        themeMode: ThemeService().theme,
-        home: const HomePage());
-  }
+@override
+Widget build(BuildContext context) {
+return GetMaterialApp(
+title: 'Agendamento',
+debugShowCheckedModeBanner: false,
+theme: Themes.light,
+darkTheme: Themes.dark,
+themeMode: ThemeService().theme,
+home: const HomePage(),
+);
 }
+}
+
+
+
+
